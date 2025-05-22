5acc0908530d3f8c1c7b02c172b79c0b5249e8e7
SylviaRemington <sylviaremington@gmail.com>
2025-05-22 13:41:01 -0700

    importing express & configuring API & creating listening for requests on port 3000

---
+++ server.js
@@ -0,0 +1,14 @@
+// Importing Express
+const express = require('express')
+
+// Creating an Express Application/App - This configures to create API
+const app = express()
+
+// Defining the routes here:
+
+
+
+// Listening for requests on port 3000
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
